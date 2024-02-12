a8d02141502f5ee51c1a67c87965d8637f3874de
Ajay chaudhary <Mettalic.Azayy@gmail.com>
2024-02-12 13:08:00 +0545

    first commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setNote(prevNote => ({
+      ...prevNote,
+      [name]: value
+    }));
+  }
+
+  function submitNote(e) {
+    props.addOn(note);
+    setNote({
+        title: "",
+        content: ""
+    })
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note ..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
